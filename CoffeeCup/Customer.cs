0a02611f279779cbb50199c051040dab276cc0c4
Kiranmaye Lankalapalli <kirru.n@outlook.com>
2017-05-26 16:46:55 -0700

    rebuilding

--- CoffeeCup/Customer.cs
+++ CoffeeCup/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace CoffeeCup
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set;}
+        [Key]
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Address { get; set; }
